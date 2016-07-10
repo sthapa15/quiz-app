071253454bda9b364300fc007a05f9812d3f9a74
sthapa15 <st2fd@virginia.edu>
2016-07-09 18:23:17 -0700

    add intent to share score in email

--- app/src/main/java/com/example/android/harrypottertrivia/MainActivity.java
+++ app/src/main/java/com/example/android/harrypottertrivia/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.harrypottertrivia;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     int score = 0; //counter to keep track of score
     int totalScore = 0;
+    CharSequence total = "";
 
 
     @Override
@@ -23,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Method to calculate score for each question
      * Could be done another way to be more efficient - array list
+     *
      * @param view
      */
     public void submitQuiz(View view) {
 
-      //  int sum=0;
+        //  int sum=0;
 
         //int[] scoret = {question1(),question2() };
 
@@ -42,14 +46,27 @@ public class MainActivity extends AppCompatActivity {
         int q9 = question9();
         int q10 = question10();
 
-      //  for (int i = 0; i<=scoret.length; i++){
-      //      sum += i;
-     //   }
+        //  for (int i = 0; i<=scoret.length; i++){
+
         totalScore = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10;
-        CharSequence total = "Your score is: " + totalScore;
+        total = "Your score is: " + totalScore;
         Toast.makeText(view.getContext(), total, Toast.LENGTH_SHORT).show();
         score = 0;
 
+    }
+
+    public void sendQuizScore(View view) {
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Quiz Score");
+        intent.putExtra(Intent.EXTRA_TEXT, total);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+
+            startActivity(intent);
+
+        }
     }
 
     private int question1() {
@@ -89,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         View answerq3 = question3.findViewById(R.id.q3_ans4);
         int idAns3 = question3.indexOfChild(findViewById(question3.getCheckedRadioButtonId()));
         int indexAns3 = question3.indexOfChild(answerq3);
-        if (idAns3 == indexAns3){
+        if (idAns3 == indexAns3) {
             score = 1;
         } else {
             score = 0;
@@ -97,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         return score;
     }
 
-    private int question4(){
+    private int question4() {
 
         //check to see if answers 2,3,5 are checked
 
@@ -110,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         CheckBox ans5 = (CheckBox) findViewById(R.id.q4_ans5);
         boolean is5Checked = ans5.isChecked();
 
-        if (is2checked && is3Checked && is5Checked){
+        if (is2checked && is3Checked && is5Checked) {
             score = 1;
         } else {
             score = 0;
@@ -118,13 +135,13 @@ public class MainActivity extends AppCompatActivity {
         return score;
     }
 
-    private int question5(){
+    private int question5() {
         RadioGroup question5 = (RadioGroup) findViewById(R.id.q5_rg);
         //option 4 - ans4 - 4 privet drive
         View answerq5 = question5.findViewById(R.id.q5_ans4);
         int idAns5 = question5.indexOfChild(findViewById(question5.getCheckedRadioButtonId()));
         int indexAns5 = question5.indexOfChild(answerq5);
-        if (idAns5 == indexAns5){
+        if (idAns5 == indexAns5) {
             score = 1;
         } else {
             score = 0;
@@ -133,13 +150,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private int question6(){
+    private int question6() {
         RadioGroup question6 = (RadioGroup) findViewById(R.id.q6_rg);
         //option 3 - ans3 - toad
         View answerq6 = question6.findViewById(R.id.q6_ans3);
         int idAns6 = question6.indexOfChild(findViewById(question6.getCheckedRadioButtonId()));
         int indexAns6 = question6.indexOfChild(answerq6);
-        if (idAns6 == indexAns6){
+        if (idAns6 == indexAns6) {
             score = 1;
         } else {
             score = 0;
@@ -147,13 +164,13 @@ public class MainActivity extends AppCompatActivity {
         return score;
     }
 
-    private int question7(){
+    private int question7() {
         RadioGroup question7 = (RadioGroup) findViewById(R.id.q7_rg);
         //option 1 - ans1 - true
         View answerq7 = question7.findViewById(R.id.q7_ans1);
         int idAns7 = question7.indexOfChild(findViewById(question7.getCheckedRadioButtonId()));
         int indexAns7 = question7.indexOfChild(answerq7);
-        if (idAns7 == indexAns7){
+        if (idAns7 == indexAns7) {
             score = 1;
         } else {
             score = 0;
@@ -161,13 +178,13 @@ public class MainActivity extends AppCompatActivity {
         return score;
     }
 
-    private int question8(){
+    private int question8() {
         RadioGroup question8 = (RadioGroup) findViewById(R.id.q8_rg);
         //option 4 - ans4 - snape
         View answerq8 = question8.findViewById(R.id.q8_ans2);
         int idAns8 = question8.indexOfChild(findViewById(question8.getCheckedRadioButtonId()));
         int indexAns8 = question8.indexOfChild(answerq8);
-        if (idAns8 == indexAns8){
+        if (idAns8 == indexAns8) {
             score = 1;
         } else {
             score = 0;
@@ -175,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
         return score;
     }
 
-    private int question9(){
+    private int question9() {
         //1, 2, 4, 6, 7
 
         CheckBox ans1 = (CheckBox) findViewById(R.id.q9_ans1);
@@ -193,7 +210,7 @@ public class MainActivity extends AppCompatActivity {
         CheckBox ans7 = (CheckBox) findViewById(R.id.q9_ans7);
         boolean is7Checked = ans7.isChecked();
 
-        if (is1checked && is2Checked && is4Checked && is6checked && is7Checked){
+        if (is1checked && is2Checked && is4Checked && is6checked && is7Checked) {
             score = 1;
         } else {
             score = 0;
@@ -201,11 +218,11 @@ public class MainActivity extends AppCompatActivity {
         return score;
     }
 
-    private int question10(){
+    private int question10() {
 
         EditText answerq10 = (EditText) findViewById(R.id.q10_ans);
         String userAns = answerq10.getText().toString().toLowerCase();
-        if (userAns.equals("expelliarmus")){
+        if (userAns.equals("expelliarmus")) {
             score = 1;
         } else {
             score = 0;
@@ -213,9 +230,6 @@ public class MainActivity extends AppCompatActivity {
         return score;
 
     }
-
-
-
 
 
 }
